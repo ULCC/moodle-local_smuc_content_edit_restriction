6ef8b1ce0e725b28381b9cd8f5f35d55221d63d4
Nigel Daley <nigel.daley@cosector.com>
2025-06-30 16:28:32 +0100

    43718 - added capability check code

--- lib.php
+++ lib.php
@@ -21,16 +21,20 @@ function local_smuc_content_edit_restriction_extend_navigation($nav) {
 
     if ($path[1] == "course") {
 
+        is_siteadmin();
+
         $courseid   =   required_param('id',PARAM_INT);
 
         $contentfunctions       =   new     \local_smuc_content_edit_restriction\content_edit_restriction();
 
-        if ($contentfunctions->is_restricted_course($courseid)) {
+        if ($contentfunctions->is_restricted_course($courseid) && !is_siteadmin($USER->id) && !has_capability('local/smuc_content_edit_restriction:overriderestriction', context_course::instance($COURSE->id))) {
             if ($pagepath == "/course/edit.php") {
 
-                $PAGE->requires->css(new \moodle_url('/local/smuc_content_edit_restriction/style/restrictions.css'));
+                $PAGE->requires->css(new \moodle_url('/local/smuc_content_edit_restriction/styles/restrictions.css'));
                 $PAGE->requires->jquery();
                 $PAGE->requires->js('/local/smuc_content_edit_restriction/js/course_settings_restrictions.js', array());
+
+
 
             } else if ($pagepath == "/course/view.php") {
 
